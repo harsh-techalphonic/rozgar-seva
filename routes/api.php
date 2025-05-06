51905ba42b8de6b0f19e5c87fbea4c1c4f7c15d3
harsh-techalphonic <harsh.techalphonic@gmail.com>
2025-05-06 10:15:49 +0530

    working on regest

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(LoginController::class)->name('main.')->group(function () {
+Route::controller(LoginController::class)->group(function () {
     Route::post('/login', 'login')->name('login');
     Route::post('/register', 'register')->name('register');
 });
